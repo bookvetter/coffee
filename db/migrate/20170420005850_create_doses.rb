3702a4e91fd0e6e32cf2cd90001eefddd326d168
Ashraf Nazar <ashraf.caspian@gmail.com>
2017-04-19 21:16:35 -0400

    added attribute of dose

--- db/migrate/20170420005850_create_doses.rb
+++ db/migrate/20170420005850_create_doses.rb
@@ -1,6 +1,7 @@
 class CreateDoses < ActiveRecord::Migration[5.0]
   def change
     create_table :doses do |t|
+      t.string :description
       t.references :drink, foreign_key: true
       t.references :ingredient, foreign_key: true
 
